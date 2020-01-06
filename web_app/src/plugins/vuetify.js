eebf025e2a459e9d3810118b31570a3a936c9d70
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2020-01-06 07:07:17 -0300

    web_app: Update dark theme

--- web_app/src/plugins/vuetify.js
+++ web_app/src/plugins/vuetify.js
@@ -15,10 +15,13 @@ export default new Vuetify({
         error: colors.red.accent3
       },
       dark: {
-        primary: colors.purple.darken1,
-        secondary: colors.grey.darken1,
-        accent: colors.shades.black,
-        error: colors.red.accent3
+        primary: colors.deepPurple.base,
+        secondary: colors.purple.base,
+        accent: colors.pink.base,
+        error: colors.red.base,
+        warning: colors.amber.base,
+        info: colors.cyan.base,
+        success: colors.lightGreen.base
       }
     }
   }
