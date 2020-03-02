52573e61625d121297b3589f4e49afde015ce1d7
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2020-03-01 22:15:08 -0300

    balanar/assets: Reorganize images and add helpers

---
+++ balanar/src/assets/helpers.js
@@ -0,0 +1,60 @@
+import { assign } from "lodash/object";
+
+export function image(path) {
+  return require(`@/assets/images/${path}`);
+}
+
+export const HERO_IMAGE_ORIENTATIONS = ["portrait", "landscape", "icon"];
+export const HERO_IMAGE_SIZES = ["regular", "large"];
+export const DEFAULT_HERO_IMAGE_ORIENTATION = "landscape";
+export const DEFAULT_HERO_IMAGE_SIZE = "regular";
+
+const HERO_PLACEHOLDER_IMAGES = {
+  "portrait.regular": "heroes/default_portrait.png",
+  "portrait.large": "heroes/default_portrait.png",
+  "landscape.regular": "heroes/default_landscape.png",
+  "landscape.large": "heroes/default_landscape.png",
+};
+
+const HERO_IMAGES = {
+  "icon": hero => `heroes/vpk/icons/${hero.name}_png.png`,
+  "portrait.regular": hero => `heroes/vpk/selection/${hero.name}_png.png`,
+  "portrait.large": hero => `heroes/cdn/vert/${hero.name}.jpg`,
+  "landscape.regular": hero => `heroes/vpk/${hero.name}_png.png`,
+  "landscape.large": hero => `heroes/cdn/full/${hero.name}.png`,
+};
+
+function heroImageVersion({
+  orientation = DEFAULT_HERO_IMAGE_ORIENTATION,
+  size = DEFAULT_HERO_IMAGE_SIZE,
+} = {}) {
+  if (orientation === "icon" || orientation.indexOf(".") >= 0) {
+    return orientation;
+  }
+
+  return `${orientation}.${size}`;
+}
+
+export function heroPlaceholderImage(options = {}) {
+  const version = heroImageVersion(options);
+  const path = HERO_PLACEHOLDER_IMAGES[version];
+
+  return path && image(path);
+}
+
+export function heroImage(hero, options = {}) {
+  options = assign({ placeholder: true }, options);
+
+  if (hero == null) {
+    if (options.placeholder === true) {
+      return heroPlaceholderImage(options);
+    } else if (options.placeholder) {
+      return image(options.placeholder);
+    }
+  }
+
+  const version = heroImageVersion(options);
+  const pathFn = HERO_IMAGES[version];
+
+  return pathFn && image(pathFn(hero));
+}
