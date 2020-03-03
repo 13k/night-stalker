fac316b767f484ad4b982c8e18f0505a6ed1fea7
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2020-03-03 16:29:18 -0300

    models: Remove image fields from Hero

--- models/hero.go
+++ models/hero.go
@@ -4,15 +4,10 @@ var HeroModel Model = (*Hero)(nil)
 
 type HeroID uint64
 
-// Hero ...
 type Hero struct {
-	ID               HeroID `gorm:"column:id;primary_key"`
-	Name             string `gorm:"column:name;size:255;unique_index;not null"`
-	LocalizedName    string `gorm:"column:localized_name;size:255;not null"`
-	ImageFullURL     string `gorm:"column:image_full_url"`
-	ImageLargeURL    string `gorm:"column:image_large_url"`
-	ImageSmallURL    string `gorm:"column:image_small_url"`
-	ImagePortraitURL string `gorm:"column:image_portrait_url"`
+	ID            HeroID `gorm:"column:id;primary_key"`
+	Name          string `gorm:"column:name;size:255;unique_index;not null"`
+	LocalizedName string `gorm:"column:localized_name;size:255;not null"`
 	Timestamps
 }
 
