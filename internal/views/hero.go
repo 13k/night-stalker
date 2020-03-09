c580433cbd81f4274d7e395dd1c13ea3e985f9bf
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2020-03-09 09:44:31 -0300

    views: Add details to `Hero`

--- internal/views/hero.go
+++ internal/views/hero.go
@@ -9,10 +9,17 @@ import (
 
 func NewHero(h *models.Hero) *nspb.Hero {
 	return &nspb.Hero{
-		Id:            uint64(h.ID),
-		Name:          h.Name,
-		Slug:          h.Slug,
-		LocalizedName: h.LocalizedName,
+		Id:                 uint64(h.ID),
+		Name:               h.Name,
+		Slug:               h.Slug,
+		LocalizedName:      h.LocalizedName,
+		Aliases:            h.Aliases,
+		Roles:              h.Roles,
+		RoleLevels:         h.RoleLevels,
+		Complexity:         int64(h.Complexity),
+		Legs:               int64(h.Legs),
+		AttributePrimary:   h.AttributePrimary,
+		AttackCapabilities: h.AttackCapabilities,
 	}
 }
 
