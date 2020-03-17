02899084e9a9e09ffb913651c21d6575bd1fea7e
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2020-03-17 14:39:05 -0300

    tools: Remove protoc-gen-go-json and update protoc-gen-go

--- tools.go
+++ tools.go
@@ -4,7 +4,6 @@ package tools
 
 import (
 	_ "github.com/golang-migrate/migrate/v4/cmd/migrate"
-	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/markbates/pkger/cmd/pkger"
-	_ "github.com/mitchellh/protoc-gen-go-json"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
