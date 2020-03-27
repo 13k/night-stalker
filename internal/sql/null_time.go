f4fd7bc14c91e9bb8183a35aeaa15175405ef5cb
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2020-03-27 04:56:32 -0300

    sql: Add NullTime helpers

---
+++ internal/sql/null_time.go
@@ -0,0 +1,58 @@
+package sql
+
+import (
+	"database/sql"
+	"time"
+
+	pbt "github.com/golang/protobuf/ptypes"
+	pbts "github.com/golang/protobuf/ptypes/timestamp"
+
+	nsjson "github.com/13k/night-stalker/internal/json"
+)
+
+var zeroNullTime = sql.NullTime{}
+
+func NullTimeUnix(sec int64) sql.NullTime {
+	if sec == 0 {
+		return zeroNullTime
+	}
+
+	return sql.NullTime{
+		Time:  time.Unix(sec, 0),
+		Valid: true,
+	}
+}
+
+func NullTimeEqual(left, right sql.NullTime) bool {
+	if !left.Valid && !right.Valid {
+		return true
+	}
+
+	if left.Valid && right.Valid {
+		return left.Time.Equal(right.Time)
+	}
+
+	return false
+}
+
+// NullTimeProto converts a NullTime into a protobuf Timestamp.
+//
+// Returns nil with nil error if the given NullTime is invalid.
+func NullTimeProto(t sql.NullTime) (*pbts.Timestamp, error) {
+	if !t.Valid {
+		return nil, nil
+	}
+
+	return pbt.TimestampProto(t.Time)
+}
+
+func NullTimeFromUnixJSON(jt *nsjson.UnixTime) sql.NullTime {
+	if jt == nil {
+		return zeroNullTime
+	}
+
+	return sql.NullTime{
+		Time:  jt.Time,
+		Valid: !jt.IsZero(),
+	}
+}
